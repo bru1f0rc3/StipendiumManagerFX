40fdb36da0eef71e2c55e841eab15df6e0c43d11
bru1f0rc3 <yankeyanke793@gmail.com>
2025-11-16 14:50:06 +0300

    application ready

--- src/main/java/ru/demo/demo2/HelloApplication.java
+++ src/main/java/ru/demo/demo2/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-menu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 1000);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 850);
         stage.setTitle("Учёт стипендий и соцподдержки студентов");
         stage.setScene(scene);
         stage.show();
--- src/main/java/ru/demo/demo2/controller/MainMenuController.java
+++ src/main/java/ru/demo/demo2/controller/MainMenuController.java
@@ -1,9 +1,12 @@
 package ru.demo.demo2.controller;
 
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.Node;
+import javafx.util.Duration;
 
 public class MainMenuController {
     
@@ -11,12 +14,52 @@ public class MainMenuController {
     private BorderPane mainContainer;
     
     @FXML
+    private VBox drawer;
+    
+    private boolean isMenuOpen = false;
+    
+    @FXML
+    public void initialize() {
+        loadView("/ru/demo/demo2/calculation-view.fxml");
+    }
+    
+    @FXML
+    public void toggleMenu() {
+        if (isMenuOpen) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
+    }
+    
+    private void openMenu() {
+        isMenuOpen = true;
+        drawer.setManaged(true);
+        drawer.toFront();
+        
+        TranslateTransition slideIn = new TranslateTransition(Duration.millis(250), drawer);
+        slideIn.setToX(0);
+        slideIn.play();
+    }
+    
+    private void closeMenu() {
+        isMenuOpen = false;
+        
+        TranslateTransition slideOut = new TranslateTransition(Duration.millis(250), drawer);
+        slideOut.setToX(-200);
+        slideOut.setOnFinished(e -> drawer.setManaged(false));
+        slideOut.play();
+    }
+    
+    @FXML
     private void onCalculationModuleClick() {
+        closeMenu();
         loadView("/ru/demo/demo2/calculation-view.fxml");
     }
     
     @FXML
     private void onPayrollModuleClick() {
+        closeMenu();
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/ru/demo/demo2/payroll-view.fxml"));
             Node view = loader.load();
